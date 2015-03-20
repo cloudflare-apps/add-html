2e81bd8e613bcf40abe7060ee43f536a67d8c0f5
Zack Bloom <zackbloom@gmail.com>
2015-03-19 21:12:51 -0400

    Execute included scripts

--- helper.js
+++ helper.js
@@ -8,6 +8,23 @@
       var el = Eager.createElement(options.blocks[i].location);
 
       el.innerHTML = options.blocks[i].code;
+
+      var scripts = el.querySelectorAll('script');
+      if (scripts){
+        for (var j=0; j < scripts.length; j++){
+          var newScript = document.createElement('script');
+          for (var k=scripts[j].attributes.length; k--;){
+              var attr = scripts[j].attributes[k];
+
+              if (attr.specified)
+                newScript.setAttribute(attr.name, attr.value);
+          }
+
+          newScript.innerHTML = scripts[j].innerHTML;
+
+          el.replaceChild(newScript, scripts[j]);
+        }
+      }
     }
   }
 
